2f682414aa74f120267cffc518a295bc698edd43
rabrahm <rbrahm@gmail.com>
2015-12-17 10:01:08 -0300

    Update setup2.py
    include paths to libraries in Mac OS

--- setup2.py
+++ setup2.py
@@ -1,4 +1,4 @@
 from distutils.core import setup, Extension
 
-module =Extension('Cfunctions', sources = ['Cfunctions.c'],libraries=['gsl','gslcblas','m'])
+module =Extension('Cfunctions', sources = ['Cfunctions.c'],libraries=['gsl','gslcblas','m'],include_dirs=['/usr/local/include/', '/anaconda/lib/python2.7/site-packages/numpy/core/include/'], library_dirs=['/usr/local/lib/'])
 setup(name = 'Funciones creadas por Nestor: Extensiones de C/Python', version = '1.0', ext_modules = [module])

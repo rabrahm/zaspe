cf2d5ec7f295747f128e5111b2b45f52ae1e5193
rabrahm <rbrahm@gmail.com>
2015-12-17 10:03:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup, Extension
 
-module =Extension('integration', sources = ['integration.c'],libraries=['gsl','gslcblas','m'])
+module =Extension('integration', sources = ['integration.c'],libraries=['gsl','gslcblas','m'],include_dirs=['/usr/local/include/', '/anaconda/lib/python2.7/site-packages/numpy/core/include/'], library_dirs=['/usr/local/lib/'])
 setup(name = 'Funciones creadas por R. Brahm: Extensiones de C/Python', version = '1.0', ext_modules = [module])

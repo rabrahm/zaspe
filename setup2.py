e20cc86791b67fcc458009f5d1575de800bbef0a
nespinoza <nestor.espinozap@gmail.com>
2015-12-17 10:32:05 -0300

    Fixed bug on setup.py files.

--- setup2.py
+++ setup2.py
@@ -1,4 +1,5 @@
+import numpy as np
 from distutils.core import setup, Extension
 
-module =Extension('Cfunctions', sources = ['Cfunctions.c'],libraries=['gsl','gslcblas','m'],include_dirs=['/usr/local/include/', '/anaconda/lib/python2.7/site-packages/numpy/core/include/'], library_dirs=['/usr/local/lib/'])
+module =Extension('Cfunctions', sources = ['Cfunctions.c'],libraries=['gsl','gslcblas','m'],include_dirs=[np.get_include(),'/usr/local/include/', '/anaconda/lib/python2.7/site-packages/numpy/core/include/'], library_dirs=['/usr/local/lib/'])
 setup(name = 'Funciones creadas por Nestor: Extensiones de C/Python', version = '1.0', ext_modules = [module])
